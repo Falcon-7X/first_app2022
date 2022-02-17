dcaf8354ea55f444146eec0ef46f6f39f97d3777
Coding-MacBook <328458211@qq.com>
2022-02-17 14:45:47 +0800

    adjust sqlite3 and pg

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_02_16_104136) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -50,7 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_16_104136) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.integer "topic_id", null: false
+    t.bigint "topic_id", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
